3df7cf3ee8a6cd5d6369b8edf38c8f825477606e
damian <dfauth@gmail.com>
2023-04-14 07:24:08 +0800

    corrected RoC calculation

--- src/main/java/com/github/dfauth/ta/functions/RateOfChange.java
+++ src/main/java/com/github/dfauth/ta/functions/RateOfChange.java
@@ -1,37 +1,21 @@
 package com.github.dfauth.ta.functions;
 
 import java.math.BigDecimal;
-import java.util.LinkedList;
+import java.math.RoundingMode;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
-
-import static com.github.dfauth.ta.functions.Accumulator.BD_ACCUMULATOR;
-import static java.lang.reflect.Array.newInstance;
 
 public class RateOfChange {
 
     public static Function<BigDecimal, Optional<BigDecimal>> roc() {
-        return roc(1);
-    }
-
-    public static Function<BigDecimal, Optional<BigDecimal>> roc(int period) {
-        return roc(period, BD_ACCUMULATOR);
-    }
-
-    public static <T extends Number, R extends Number> Function<T, Optional<R>> roc(int period, Accumulator<T,R> ops) {
-        LinkedList<T> l = new LinkedList<>();
+        final AtomicReference<BigDecimal> previous = new AtomicReference<>(null);
         return t -> {
-            l.add(t);
-            if(l.size() < period+1) {
-                return Optional.empty();
-            } else {
-                if(l.size() > period+1) {
-                    l.remove(0);
-                }
-                ops.set(l.get(period));
-                ops.subtract(l.get(0));
-                return Optional.of(ops.divide(period));
+            try {
+                return Optional.ofNullable(previous.get())
+                        .map(p -> t.divide(p, 3, RoundingMode.HALF_UP).subtract(BigDecimal.ONE));
+            } finally {
+                previous.set(t);
             }
         };
     }
--- src/test/java/com/github/dfauth/ta/functions/ROCTest.java
+++ src/test/java/com/github/dfauth/ta/functions/ROCTest.java
@@ -3,12 +3,11 @@ package com.github.dfauth.ta.functions;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import static com.github.dfauth.ta.functions.MovingAverages.sma;
 import static com.github.dfauth.ta.functions.RateOfChange.roc;
 import static org.junit.Assert.assertEquals;
 
@@ -20,9 +19,10 @@ public class ROCTest {
     @Test
     public void testIt() {
         assertEquals(
-                List.of(ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP, ONE_3DP),
+                Stream.of(1.000, 0.500, 0.333, 0.250, 0.200, 0.167, 0.143, 0.125, 0.111, 0.100, 0.091, 0.083, 0.077, 0.071, 0.067).map(BigDecimal::valueOf).map(bd -> bd.setScale(3)).collect(Collectors.toList()),
                 IntStream.of(TEST_DATA).boxed()
                         .map(BigDecimal::valueOf)
+                        .map(bd -> bd.setScale(3))
                         .map(roc())
                         .flatMap(Optional::stream)
                         .collect(Collectors.toList())
